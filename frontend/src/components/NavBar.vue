2b11484e221640a9acb921238c2f8c882ab136fc
Kae <73745621+Kae-Tempest@users.noreply.github.com>
2023-01-06 16:37:39 +0100

    Update NavBar.vue
    change css class to custom tailwind class

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="w-auto bg-ctp-crust flex justify-between items-center pl-4 pr-32">
         <router-link to="/"><div class="flex gap-4">
-            <img src="../assets/Logo.png" alt="logo" class="h-71">
+            <img src="../assets/Logo.png" alt="logo" class="h-[71px]">
             <h1 class="text-ctp-text text-xxl">Métépaslà</h1>
         </div></router-link>
         <div>
