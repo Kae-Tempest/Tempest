a24470e05f7bda48ac605da307e1e90d0d3d19a6
Kae <73745621+Kae-Tempest@users.noreply.github.com>
2023-01-06 16:34:54 +0100

    Update Express App
    secure sql query
    Add Type for query return
    add put path for updateSensor

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import { hashSync, compareSync } from 'bcrypt';
-import { User, Sensor } from './types';
+import { User, Sensor, Data } from './types';
 import { logger } from './middleware/logger';
-
 const { db } = require('./databases/connect.js');
 const app: Application = express();
+
+
+
 app.use(express.json());
 app.use(cors());
 
@@ -13,7 +15,6 @@ app.use(cors());
 
 app.post('/login', async (req: Request, res: Response) => {
 	const Members: User[] = await db.query(`select * from users where name = ? order by id desc`, [req.body.name]);
-	if (Members.length === 0) return res.status(401).send({ msg: "Name doesn't existe" });
 	let MemberFound = false;
 	Members.forEach((member) => {
 		if (member.name === req.body.name && compareSync(req.body.password, member.password)) {
@@ -43,7 +44,7 @@ app.post('/signup', async (req: Request, res: Response) => {
 	});
 
 	let hashPassword = hashSync(newMember.password, 10);
-	db.query(`insert into users (name, password, email) values ('${newMember.name}', '${hashPassword}', '${newMember.email}')`);
+	db.query(`insert into users (name, password, email) values (?,?,?)`, [newMember.name, hashPassword, newMember.email]);
 	res.sendStatus(200);
 });
 
@@ -54,11 +55,11 @@ app.post('/data/:id', (req: Request, res: Response) => {
 		Humidity: req.body.hum,
 		date: Date.now(),
 	};
-	db.query(`insert into data (sensor_id, temperature, humidity, mesured_at) values (${newData.id}, ${newData.temperature}, ${newData.Humidity}, ${newData.date})`);
+	db.query(`insert into data (sensor_id, temperature, humidity, mesured_at) values (?,?,?,?)`, [newData.id, newData.temperature, newData.Humidity, newData.date]);
 	return res.send(200);
 });
 app.post('/createSensor', async (req: Request, res: Response) => {
-	const Sensors = await db.query(`select * from sensor`);
+	const Sensors: Sensor[] = await db.query(`select * from sensor`);
 	const newSensor = {
 		id: req.body.id,
 		name: req.body.name,
@@ -69,24 +70,38 @@ app.post('/createSensor', async (req: Request, res: Response) => {
 		if (newSensor.id === sensor.id) {
 			return res.status(401).send({ msg: 'Sensor already exist' });
 		} else {
-			db.query(`insert into sensor (id,sensor_name, longitude, latitude) values (${newSensor.id},'${newSensor.name}',${newSensor.longitude}, ${newSensor.latitude})`);
+			db.query(`insert into sensor (id,sensor_name, longitude, latitude) values (?,?,?,?)`, [newSensor.id, newSensor.name, newSensor.longitude, newSensor.latitude]); 
 			return res.status(200);
 		}
 	});
 });
-
+app.put('/updateSensor/:id', async (req: Request, res: Response) => {
+	const sensor: Sensor[] = await db.query(`select * from sensor where id = ?`, [req.params.id]);
+	const newSensor = {
+		id: req.params.id,
+		name: req.body.name,
+		longitude: req.body.longitude,
+		latitude: req.body.latitude,
+	};	
+	if (newSensor.id === JSON.stringify(sensor[0].id)) {
+		db.query(`update sensor set sensor_name = ? , longitude = ? , latitude = ? where id = ?`, [newSensor.name, newSensor.longitude, newSensor.latitude, newSensor.id]);
+		return res.sendStatus(200);
+	} else {
+		return res.status(401).send({ msg: 'Sensor not found' });
+	}
+});
 app.get('/report/:id', async (req: Request, res: Response) => {
-	const data = await db.query(`select * from data where sensor_id = ${req.params.id}`);
+	const data: Data[] = await db.query(`select * from data where sensor_id = ?`, [req.params.id]);
 	return res.send(data).status(200);
 });
 
 app.get('/sensor', async (req: Request, res: Response) => {
-	const data = await db.query(`select * from sensor`);
+	const data: Sensor[] = await db.query(`select * from sensor`);
 	return res.send(data).status(200);
 });
 
 app.get('/sensorPosition/:id', async (req: Request, res: Response) => {
-	const Position = await db.query(`select longitude, latitude from sensor where id = ${req.params.id}`);
+	const Position: Number[] = await db.query(`select longitude, latitude from sensor where id = ?`, [req.params.id]);
 	return res.send(Position).status(200);
 });
 
