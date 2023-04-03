af5ba0da1b5949457ab3fc110b2d318c974ed6ea
Kae <73745621+Kae-Tempest@users.noreply.github.com>
2023-04-03 22:15:45 +0200

    Delete frontend-react directory

--- frontend-react/src/components/reusable/CustomButton.jsx
+++
@@ -1,6 +0,0 @@
-export function CustomButton({bgColor, textColor, size, name, font}) {
-  return (
-    <button className={`${bgColor} ${textColor} ${size} ${font} py-2 text-center leading-tight text-xl md:text-base mx-auto mt-10 mb-4 border rounded-xl`}>{name}</button>
-  )
-}
-
